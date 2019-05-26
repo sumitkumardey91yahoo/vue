daca66b79deb3e96a73384481b038c4f25863aee
sumitkumar91 <sumit.kumar@mobiotics.com>
2019-05-26 18:35:41 +0530

    Updates

--- static/js/0.ad8b3ac422a0b7a99792.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([0],{EXDq:function(e,t,n){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var r={render:function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("\n     test\n")])},staticRenderFns:[]},s=n("VU/8")({},r,!1,null,null,null);t.default=s.exports}});
-//# sourceMappingURL=0.ad8b3ac422a0b7a99792.js.map
